f4a289caa2a3544ae54f9a88f310154ccdfd11cb
Narve Sætre <narve@dv8.no>
2022-02-08 11:20:31 +0100

    merge joachims work, fix form submit, experiment with html/css

--- src/components/OrderLine.vue
+++ src/components/OrderLine.vue
@@ -3,10 +3,10 @@
 import {ref} from "vue";
 
 const data = ref([]);
-const newItem = ref([]);
+const newItem = ref({});
 
 const createNew = (args: any) => {
-  console.log('create new', args);
+  console.log('create new', JSON.stringify(newItem.value, null, ' '));
 };
 
 const refresh = (args?: any) => {
@@ -20,31 +20,68 @@ const remove = (args?: any) => {
 await refresh();
 
 export default {
-  methods() {
-    return {
-      createNew,
-      refresh,
-    }
+  methods: {
+    // return {
+    async createNew(args: any) {
+      await createNew(args);
+    },
+    async refresh(args: any) {
+      await refresh(args);
+    },
   },
   setup() {
     return {
       data, newItem
     };
-  }, 
+  },
   name: 'Bestillinger',
   path: '/orderline'
 }
 </script>
 
+<style>
+/*label span {min-width: 10em; display: inline-block; }*/
+/*fieldset label input {*/
+/*}*/
+
+label {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+label > input {
+  width: 200px;
+}
+
+</style>
+
 <template>
   <h2>Bestillinger</h2>
 
-  <form  @submit.prevent="createNew">
+  <form @submit.prevent="createNew">
     <fieldset>
       <legend>Registrer ny bestilling</legend>
-      <label>Kundens navn: </label>
-      <input v-model="newItem['name']">
-      <input type="submit">
+      <div>
+        <label>Navn:</label>
+        <input v-model="newItem['name']">
+      </div>
+      <label>
+        <span>Navn:</span>
+        <input v-model="newItem['name']">
+      </label>
+      <label>Adresse:
+        <input v-model="newItem['address']">
+      </label>
+      <label>Antall:
+        <input type="number" v-model="newItem['number_of_items']">
+      </label>
+      <label>Kommentarer:
+        <input v-model="newItem['name']">
+      </label>
+      <input type="submit" value="Registrer">
     </fieldset>
   </form>
 
